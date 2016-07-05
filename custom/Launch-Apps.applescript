e7413d2d7e128ea81d306ae415b81264098cae09
Lew Goettner <Lewis_Goettner@comcast.com>
2016-07-05 10:35:20 -0400

    Updates

--- custom/Launch-Apps.applescript
+++ custom/Launch-Apps.applescript
@@ -1,6 +1,6 @@
 ignoring application responses
 	tell application "Twitter" to launch
 	tell application "LimeChat" to launch
+	tell application "Slack" to launch
 	tell application "Microsoft Outlook" to launch
-	tell application "HipChat" to launch
 end ignoring
--- custom/osx-shortcuts.zsh
+++ custom/osx-shortcuts.zsh
@@ -6,14 +6,11 @@ if test "$VENDOR" = "apple"; then
 
 	# LS USB
 	alias lsusb='system_profiler SPUSBDataType'
-	
+
 	# Move windows Hi/Low (Vance)
 	alias hilo='osascript ~ZSH_CUSTOM/Hi-Low-Windows.applescript'
-	
+
 	# Launch a few of the regulars
 	alias helo='osascript ~ZSH_CUSTOM/Launch-Apps.applescript'
-
-	# Launch sublime text
-	alias subl='/Applications/Sublime\ Text\ 2.app/Contents/SharedSupport/bin/subl'
 	
 fi
